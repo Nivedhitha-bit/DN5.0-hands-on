b2ad4d42f2875385ca92db69105bc02d7466082c
Nivedhithasri S <niviselvam07@gmail.com>
2026-07-10 18:39:46 +0530

    Week 4 Exercise 2: Service Discovery and API Gateway

--- src/week4/Exercise 2 - Service Discovery and API Gateway/apigateway/src/main/java/com/cognizant/apigateway/ApiGatewayApplication.java
+++ src/week4/Exercise 2 - Service Discovery and API Gateway/apigateway/src/main/java/com/cognizant/apigateway/ApiGatewayApplication.java
@@ -1,2 +1,12 @@
-package com.cognizant.apigateway;public class ApiGatewayApplication {
+package com.cognizant.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApiGatewayApplication.class, args);
+    }
 }
